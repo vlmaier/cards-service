ebd74906e42804b1308d517b55e0013421ca9b6c
Vladas Maier <v.maier@dracoon.com>
2022-11-29 22:07:48 +0100

    Rename CardsService to CardsServiceApp

--- src/main/kotlin/com/vmaier/marvel/snap/cards/CardsServiceApp.kt
+++ src/main/kotlin/com/vmaier/marvel/snap/cards/CardsServiceApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CardsService
+class CardsServiceApp
 
 fun main(args: Array<String>) {
-    runApplication<CardsService>(*args)
+    runApplication<CardsServiceApp>(*args)
 }
