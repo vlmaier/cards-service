ec9aa7b059832500a3259ddd77a663b48ff311b9
Vladas Maier <v.maier@dracoon.com>
2022-12-12 11:25:24 +0100

    #2 Use flyway for database migration

---
+++ src/main/resources/db/migration/V1_0_0__Initial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cards (
+    id      SERIAL PRIMARY KEY,
+    ability VARCHAR(1024),
+    cost    INTEGER NOT NULL,
+    name    VARCHAR(255),
+    power   INTEGER NOT NULL,
+    url     VARCHAR(255)
+);
